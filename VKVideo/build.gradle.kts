4a2c1b1e896701089f7b6f491f9bd29cab52a112
IQ202512 <deewanaarya81@gmail.com>
2026-09-13 21:45:29 +0500

    Add VK Video provider template

---
+++ VKVideo/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "VK Video public video search provider."
+    authors = listOf("webcreaters-ux")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=vkvideo.ru&sz=%size%"
+    isCrossPlatform = true
+}
