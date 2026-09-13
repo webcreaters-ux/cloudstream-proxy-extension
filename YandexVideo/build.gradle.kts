dc1eca7ececb220b3603e3498f47d1b6a0c1f249
IQ202512 <deewanaarya81@gmail.com>
2026-09-13 21:45:50 +0500

    Add Yandex Video provider template

---
+++ YandexVideo/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Yandex Video public video search provider."
+    authors = listOf("webcreaters-ux")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=yandex.com&sz=%size%"
+    isCrossPlatform = true
+}
