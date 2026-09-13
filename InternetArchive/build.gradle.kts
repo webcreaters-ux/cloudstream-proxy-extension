9f58024ab6a39958efa1646fe676f8e1889b1679
IQ202512 <deewanaarya81@gmail.com>
2026-09-13 22:28:27 +0500

    Add Internet Archive provider module

---
+++ InternetArchive/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Internet Archive public media search and playback provider."
+    authors = listOf("webcreaters-ux")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=archive.org&sz=%size%"
+    isCrossPlatform = true
+}
