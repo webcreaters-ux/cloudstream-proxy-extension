64242ece4cee6df0d8299c1053df98f0c10b1f86
IQ202512 <deewanaarya81@gmail.com>
2026-09-15 23:23:35 +0500

    Add Wikimedia Commons video provider

---
+++ WikimediaCommons/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Public-domain and freely licensed media from Wikimedia Commons."
+    authors = listOf("webcreaters-ux")
+    status = 1
+    tvTypes = listOf("Movie", "Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=commons.wikimedia.org&sz=%size%"
+    isCrossPlatform = true
+}
