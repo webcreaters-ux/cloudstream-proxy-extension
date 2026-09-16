64e143e06291ccaff49897998960c44cf3413de0
IQ202512 <deewanaarya81@gmail.com>
2026-09-16 22:06:57 +0500

    Use CloudStream pre-release stubs for native plugin settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,8 @@ subprojects {
 
     dependencies {
         val implementation by configurations
-        implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
+        val cloudstream by configurations
+        cloudstream("com.lagradost:cloudstream3:pre-release")
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.18.3")
