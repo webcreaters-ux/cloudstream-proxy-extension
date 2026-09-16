b6f061dc0bb24d67373cfdd5c54ed17c91afef45
IQ202512 <deewanaarya81@gmail.com>
2026-09-16 16:32:44 +0500

    Fix VK Video plugin release metadata

--- VKVideo/build.gradle.kts
+++ VKVideo/build.gradle.kts
@@ -1,10 +1,10 @@
-version = 1
+version = 2
 
 cloudstream {
-    description = "VK Video public video search provider."
+    description = "VK Video public video search and playback provider."
     authors = listOf("webcreaters-ux")
     status = 1
     tvTypes = listOf("Movie", "TvSeries", "Others")
-    iconUrl = "https://www.google.com/s2/favicons?domain=vkvideo.ru&sz=%size%"
+    iconUrl = "https://vkvideo.ru/favicon.ico"
     isCrossPlatform = true
 }
