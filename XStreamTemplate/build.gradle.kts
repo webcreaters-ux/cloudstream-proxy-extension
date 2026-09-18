9d174c47e1dc8debf60bea760db59ca8f4921b0c
IQ202512 <deewanaarya81@gmail.com>
2026-09-18 11:28:53 +0500

    Add isolated XStream template: build.gradle.kts

---
+++ XStreamTemplate/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Standalone XStream-style provider template. Isolated from the other repository providers."
+    authors = listOf("webcreaters-ux")
+    status = 3
+    tvTypes = listOf("Movie", "TvSeries", "Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=xstream.cc&sz=%size%"
+    isCrossPlatform = true
+}
