cd1b242bad390908b7a8e7d712eb2da9756c9067
IQ202512 <deewanaarya81@gmail.com>
2026-09-24 23:49:08 +0500

    Add PlexFree provider

---
+++ PlexFree/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Authorized free entertainment provider: Plex"
+    authors = listOf("webcreaters-ux")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=plex.tv&sz=%size%"
+    isCrossPlatform = true
+}
