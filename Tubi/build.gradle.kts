8f1a1b8f305d1cf50285ed836e5aec9800673e3c
IQ202512 <deewanaarya81@gmail.com>
2026-09-24 23:48:53 +0500

    Add Tubi provider

---
+++ Tubi/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Authorized free entertainment provider: Tubi"
+    authors = listOf("webcreaters-ux")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=tubitv.com&sz=%size%"
+    isCrossPlatform = true
+}
