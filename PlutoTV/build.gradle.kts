cc0a987658049245764870b9199846079472e455
IQ202512 <deewanaarya81@gmail.com>
2026-09-24 23:49:01 +0500

    Add PlutoTV provider

---
+++ PlutoTV/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Authorized free entertainment provider: PlutoTV"
+    authors = listOf("webcreaters-ux")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=pluto.tv&sz=%size%"
+    isCrossPlatform = true
+}
